69604cfecec156caf4cbaf99cd638c1eabbf0b64
LeoAmby <leoamby@gmail.com>
2026-07-24 11:43:01 +0300

    addition of a mcrb favicon

---
+++ src/app/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  standalone:true,
+})
+export class Login {}
+  // userName = ""
+
+  // enterUsrName(event:Event){
+  //   console.log(event)
+  // }
+
