1af0eb3299b41e36053dfd58ee53819d1108e9fa
LeoAmby <leoamby@gmail.com>
2026-08-14 15:53:17 +0300

    forgot password component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes, RouterLink } from '@angular/router';
 import { Login } from './login/login';
 import { Learning } from './learning/learning';
+import { ForgotPassword } from './forgot-password/forgot-password';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Learning],
+  imports: [RouterOutlet, RouterLink, Login, Learning, ForgotPassword,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('dvt-metropol');
+
 }
+
+export const routes:Routes = [
+  {path: 'login', component:Login},
+  {path: 'forgot-password', component:ForgotPassword},
+
+];
--- src/app/learning/learning.ts
+++ src/app/learning/learning.ts
@@ -24,4 +24,33 @@ export class Learning {
     changeColor(event:Event ){
     this.color = (event.target as HTMLInputElement).value
     }
-}
+
+
+    display=true;
+  
+    students = [
+      {name:'Leo', age: 40, email:'leo@menubar.com'},
+      {name:'Faith', age: 30, email:'faith@menubar.com'},
+      {name:'Delan', age: 20, email:'delan@menubar.com'},
+      {name:'Amos', age: 43, email:'amos@menubar.com'},
+  
+    ]
+  
+    enterUsrName(event:Event){
+      console.log(event)
+    }
+  
+    show(){
+    this.display = true;
+  }
+  
+  hide(){
+    this.display = false;
+  }
+  
+  toggle(){
+    this.display=!this.display
+  }
+  
+  
+  }
