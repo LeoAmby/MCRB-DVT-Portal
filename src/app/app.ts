e6000c38529d181446c824f86b058c5724a6fc38
LeoAmby <leoamby@gmail.com>
2026-08-19 12:09:14 +0300

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, Routes, RouterLink } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('dvt-metropol');
-
 };
