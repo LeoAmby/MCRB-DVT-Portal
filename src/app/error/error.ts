b3d88015ef4621331c09357c16517816b0ee9627
LeoAmby <leoamby@gmail.com>
2026-08-19 11:42:15 +0300

    updates

--- src/app/error/error.ts
+++ src/app/error/error.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-// import { RouterLink } from '@angular/router';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-error',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './error.html',
   styleUrl: './error.css',
 })
