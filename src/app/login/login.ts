51d2b398c023f6dbdd0296231da9763f65d007cb
LeoAmby <leoamby@gmail.com>
2026-08-25 11:56:47 +0300

    form grouping and control

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
   standalone:true,
 })
 export class Login {
-  
+
+  loginForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+
+  })
+
+ signin(){
+  this.loginForm.value
+ } 
 }
