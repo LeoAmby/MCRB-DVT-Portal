176f358da52afd6cc5ce51ec977e274ed09e72ea
LeoAmby <leoamby@gmail.com>
2026-08-26 11:14:40 +0300

    strict angular validators

--- src/app/forgot-password/forgot-password.ts
+++ src/app/forgot-password/forgot-password.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css',
   standalone:true,
@@ -15,10 +17,14 @@ export class ForgotPassword {
   
     email: new FormControl('', [
       Validators.required,
-      Validators.email]),
+      Validators.email,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),
 
   });
  
+  get email(){
+    return this.forgotPassForm.get('email');
+  }
   forgotPass(){
     this.forgotPassForm.value
   }
