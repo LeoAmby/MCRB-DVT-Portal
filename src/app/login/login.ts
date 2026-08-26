dd5098ad7db2a298675f0ae9f3454b0a7a84dcd1
LeoAmby <leoamby@gmail.com>
2026-08-26 10:53:11 +0300

    strict validators

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-
-
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './login.html',
   styleUrl: './login.css',
   standalone:true,
@@ -16,14 +15,23 @@ export class Login {
   loginForm = new FormGroup({
     email: new FormControl('', [
       Validators.required,
-      Validators.email]),
+      Validators.email,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(6),
       Validators.maxLength(15)]),
 
   })
+
+  get email(){
+    return this.loginForm.get('email')
+  }
  
+  get password(){
+    return this.loginForm.get('password')
+  }
+
  signin(){
   console.log(this.loginForm.value);
  } 
